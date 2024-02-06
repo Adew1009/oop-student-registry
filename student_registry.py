3f5bb15873b38b25e5c3d60f0870b462da6624a5
Andrew_Dew <adew1009@gmail.com>
2024-02-06 17:47:37 -0600

    Finished coding problem, solved age setter issue

--- student_registry.py
+++ student_registry.py
@@ -1,10 +1,11 @@
 import re
 class Student:
     
-    def __init__(self, name, age=13, grade="12th"):
+    def __init__(self, name, age=13, grade="12th", subject = "General Studies"):
         self._name = name
         self._age = age
         self._grade = grade
+        self._subject = subject
     
     def __str__(self):
         return f"Student 1: Name: {self._name}, Age: {self._age}, Grade: {self._grade}"
@@ -32,7 +33,7 @@ class Student:
         {3,}: Match the previous character class (any letter) at least 3 times.
         $: End of the string.
         This regex ensures that the string consists only of letters (uppercase or lowercase) and is at least three characters long."""
-        
+
         if isinstance(new_name, str) and re.match(pattern, new_name):
             self._name = new_name
         else:
@@ -57,20 +58,19 @@ class Student:
     def set_grade(self, new_grade):
         if len(new_grade)==3 and new_grade[0]=="9" and new_grade[1:]=="th":
             self._grade = new_grade
-        elif len(new_grade)==4 and new_grade[0]=="1" and 0<=int(new_grade[1])<=2 and new_grade[3:]=="th":
+        elif len(new_grade)==4 and int(new_grade[:2]) in [10,11,12] and new_grade[2:] == "th":
             self._grade = new_grade
         else:
             print("Grade must be a string")
+    
+    def years_advanced(self):
+        advanced_grade = int(self._grade[:-2])
+        return f"{self._name} has advanced to the {advanced_grade}th"
+    
+    def subject(self):
+        return f"{self._name} is studying {self._subject}"
 
-    """
-    @get_grade.setter
-    def set_grade(self, new_grade):
-        if isinstance(new_grade, str) and new_grade.endswith('th'):
-            numeric_grade = int(new_grade[:-2])
-            if 9 <= numeric_grade <= 12:
-                self._grade = new_grade
-    """
-
+    
 
 andrew = Student("Andrew", 14, "12th")
 dave = Student("Dave", 18, "11th")
@@ -78,62 +78,12 @@ dave = Student("Dave", 18, "11th")
 # print(andrew.get_grade)
 # print(andrew.get_name)
 
-dave.set_age = 14
-dave.set_grade = "10th"
+# dave.set_age = 14
+dave.set_grade = "12th"
 dave.set_name = "David"
-print(dave._grade[0])
 print(dave.get_age)
+print(dave.years_advanced())
+print(dave.subject())
 print(dave.get_grade)
 print(dave.get_name)
     
-# class Student:
-    
-#     def __init__(self, name, age=13, grade="12th"):
-#         self._name = name
-#         self._age = age
-#         self._grade = grade
-    
-#     def __str__(self):
-#         return f"Student: Name: {self._name}, Age: {self._age}, Grade: {self._grade}"
-    
-#     @property
-#     def get_name(self):
-#         return self._name
-    
-#     @get_name.setter
-#     def set_name(self, new_name):
-#         pattern = r'^[a-zA-Z]{3,}$'  # Regex pattern
-#         if isinstance(new_name, str) and re.match(pattern, new_name):
-#             self._name = new_name
-#         else:
-#             print("Student name must be a string that is 3 characters or more and holds no spaces or special characters")
-    
-#     @property
-#     def get_age(self):
-#         return self._age
-    
-#     @get_age.setter
-#     def set_age(self, new_age):
-#         if isinstance(new_age, int) and 11 <= new_age <= 18:
-#             self._age = new_age
-#         else:
-#             print("Age must be an integer between 11 and 18") 
-    
-#     @property
-#     def get_grade(self):
-#         return self._grade
-    
-#     @get_grade.setter
-#     def set_grade(self, new_grade):
-#         if isinstance(new_grade, str):
-#             self._grade = new_grade
-#         else:
-#             print("Grade must be a string")
-
-# # Example usage:
-# student1 = Student("John")
-# print(student1)
-# student1.set_name = "Mike"
-# student1.set_age = 15
-# student1.set_grade = "10th"
-# print(student1)
